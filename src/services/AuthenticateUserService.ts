f787986da581451d675898e88a3297764e59918a
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2021-10-21 07:51:07 -0300

    Começando a salvar no Prisma 01:04:46

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,9 +1,21 @@
 import axios from 'axios';
+import { application } from 'express';
+
+interface IAccessTokenResponse{
+    access_token:string;
+}
+
+interface IUserResponse{
+    avatar_url:string,
+    login:string,
+    id:number,
+    name:string
+}
 
 class AuthenticateUserService{
     async execute(code:string){
         const url = "https://github.com/login/oauth/access_token";
-        const response = await axios.post(url,null,{
+        const {data:accessTokenResponse} = await axios.post<IAccessTokenResponse>(url,null,{
             params:{
                 client_id: process.env.GITHUB_CLIENT_ID,
                 client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -14,6 +26,13 @@ class AuthenticateUserService{
             }
         } );
 
+        const response = await axios.get<IUserResponse>('https://api.github.com/user', {
+            headers:{
+                authorization: `Bearer ${accessTokenResponse.access_token}`
+            }
+        })
+
+        const {login, id, avatar_url, name } = response.data; 
         return response.data;
     }
 }
